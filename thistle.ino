affe695d449313917c1a084e2f38cbd346dd3f32
Ayaan Grover <ayaangrover@gmail.com>
2025-07-05 18:31:14 -0700

    Update thistle.ino

--- thistle.ino
+++ thistle.ino
@@ -1,44 +1,86 @@
 const int switchPins[4] = {26, 27, 28, 29};
 const int ledPins[4] = {6, 7, 0, 1};
 
+const int maxSequenceLength = 20;
+int sequence[maxSequenceLength];
+int sequenceLength = 1;
+int userIndex = 0;
+bool gameOver = false;
+
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(switchPins[i], INPUT_PULLUP);
-  }
-  for (int i = 0; i < 4; i++) {
     pinMode(ledPins[i], OUTPUT);
     digitalWrite(ledPins[i], LOW);
   }
+  randomSeed(analogRead(0));
+  generateSequence();
 }
 
 void loop() {
-  bool swStates[4];
-  for (int i = 0; i < 4; i++) {
-    swStates[i] = (digitalRead(switchPins[i]) == LOW);
-  }
-
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(ledPins[i], LOW);
-  }
-
-  if (swStates[3]) {
+  if (gameOver) {
     for (int i = 0; i < 4; i++) {
       digitalWrite(ledPins[i], HIGH);
     }
-  }
-  else if (swStates[2]) {
-    for (int i = 0; i < 3; i++) {
-      digitalWrite(ledPins[i], HIGH);
+    delay(500);
+    for (int i = 0; i < 4; i++) {
+      digitalWrite(ledPins[i], LOW);
     }
-  }
-  else if (swStates[1]) {
-    for (int i = 0; i < 2; i++) {
-      digitalWrite(ledPins[i], HIGH);
-    }
-  }
-  else if (swStates[0]) {
-    digitalWrite(ledPins[0], HIGH);
+    delay(500);
+    return; 
   }
 
-  delay(10);
+  playSequence();
+
+  while (userIndex < sequenceLength) {
+    int pressed = getPressedButton();
+    if (pressed != -1) {
+      flashLED(pressed);
+
+      if (pressed != sequence[userIndex]) {
+        gameOver = true;
+        return;
+      }
+      userIndex++;
+    }
+  }
+
+  delay(500);
+  userIndex = 0;
+  sequenceLength++;
+  if (sequenceLength > maxSequenceLength) {
+    sequenceLength = 1;
+  }
+}
+
+void generateSequence() {
+  for (int i = 0; i < maxSequenceLength; i++) {
+    sequence[i] = random(0, 4);
+  }
+}
+
+void playSequence() {
+  for (int i = 0; i < sequenceLength; i++) {
+    flashLED(sequence[i]);
+    delay(250);
+  }
+}
+
+void flashLED(int ledIndex) {
+  digitalWrite(ledPins[ledIndex], HIGH);
+  delay(300);
+  digitalWrite(ledPins[ledIndex], LOW);
+  delay(150);
+}
+
+int getPressedButton() {
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(switchPins[i]) == LOW) {
+      while (digitalRead(switchPins[i]) == LOW) {
+        delay(10); 
+      }
+      return i;
+    }
+  }
+  return -1;
 }
